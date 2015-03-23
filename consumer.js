607bcdf7f99241b6c0e71d119ba3d336ca0588fb
Joao Oliveira <joao.fontes@digitaljump.net>
2015-03-23 14:55:20 +0000

    First commit

---
+++ consumer.js
@@ -0,0 +1,38 @@
+/**
+ * Consumer 
+ *
+ * @author João Fontes Oliveira <@Joao_Oliveira>
+ * @March 2015
+ *
+ */
+
+const net = require("net");
+
+// Create the consumer
+var consumer = net.createServer(function(conn) {
+    console.log("Consumer says: someone is calling...");
+
+    // Handle data from producer
+    conn.on("data", function(data) {
+        data = JSON.parse(data);
+        console.log("Consumer says: someone is asking for response to: %s", data.expression);
+
+        // Let's response with a hello message
+        conn.write(
+            JSON.stringify(
+                { response:  eval(data.expression.replace('=',''))}
+            )
+        );
+    });
+
+    // If connection is closed
+    conn.on("end", function() {
+        // console.log('Consumer says: Bye bye :(');
+        console.log('');
+    });
+});
+
+// Listen for connections
+consumer.listen(8084, "localhost", function () {
+    console.log("Consumer says: Bring on the expressions!");
+});
---
+++ producer.js
@@ -0,0 +1,35 @@
+/**
+ * Producer 
+ *
+ * @author João Fontes Oliveira <@Joao_Oliveira>
+ * @March 2015
+ *
+ */
+
+const net = require("net");
+
+// Create a socket that connects to the consumer
+var socket = new net.Socket();
+
+var interval = setInterval(function() {
+	socket.connect(8084, "localhost", function () {
+		var question = Math.floor((Math.random() * 100) + 1) + '+' + Math.floor((Math.random() * 100) + 1) + '=';
+	    console.log("Producer says: connection successful. Asking for " + question);
+	    socket.write(
+	    	JSON.stringify(
+	    	    { expression: question}
+	    	)
+	    );
+	});
+},100);
+
+
+
+// Let's handle the data we get from the server
+socket.on("data", function (data) {
+    data = JSON.parse(data);
+    console.log("Producer says: got answer %s", data.response);
+    console.log("")
+    // Close the connection
+    socket.end();
+});
